48021fb83ee6153c69f32f57e19e5d5f271b0553
NursaraAin <80375755+NursaraAin@users.noreply.github.com>
2023-03-23 10:20:35 +0800

    Add project files.

---
+++ SecondExcelExtract/Excel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace SecondExcelExtract
+{
+    internal class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb=excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value != null)
+            {
+                return ws.Cells[i, j].Value;
+            }
+            else
+                return "";
+        }
+        public void WriteToCell(int i,int j, string s)
+        {
+            i++;
+            j++;
+            ws.Cells[i, j].Value = s;
+        }
+        public void Save()
+        {
+            wb.Save();
+        }
+
+        public void SaveAs(string path)
+        {
+            wb.SaveAs(path);
+        }
+
+        public void Close()
+        {
+            wb.Close(SaveChanges: false);
+            excel.Quit();
+
+            Marshal.ReleaseComObject(ws);
+            Marshal.ReleaseComObject(wb);
+            Marshal.ReleaseComObject(excel);
+        }
+    }
+}
---
+++ SecondExcelExtract/Form1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SecondExcelExtract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Excel excel;
+        OpenFileDialog ofd = new OpenFileDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string fileExt = Path.GetExtension(ofd.FileName); //get the file extension
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        excel=new Excel(ofd.FileName,1);
+                        string[] fileName = ofd.FileName.Split('\\');
+                        label1.Visible = true;
+                        label1.Text = fileName[fileName.Length - 1];
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    DialogResult dialogResult = MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error
+                }
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            excel.Close();
+        }
+    }
+}
